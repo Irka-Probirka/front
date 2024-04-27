4887329e4585af388aff4415288e901bc8def7e3
Irka-Probirka <ankuznecov42@yandex.ru>
2024-04-28 04:50:54 +0500

    добавлен новый компонент span

---
+++ src/components/span.js
@@ -0,0 +1,13 @@
+
+
+const Span = ({children}) => {
+    return (
+        <span
+            className={'border-b-2 border-solid pb-px'}
+        >
+            {children}
+        </span>
+    );
+};
+
+export default Span;
